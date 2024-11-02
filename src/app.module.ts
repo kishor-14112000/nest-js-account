523137ce1a33eee40330c10875bac2973040da88
kishor-14112000 <kishorkumar14112@gmail.com>
2024-11-02 21:48:09 +0530

    packages added-1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.LOCAL_DB_HOST,
+      port: 5432,
+      username: process.env.LOCAL_DB_USER,
+      password: process.env.LOCAL_DB_PASSWORD,
+      database: 'your_database',
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
