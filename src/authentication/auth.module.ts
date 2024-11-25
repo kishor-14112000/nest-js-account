059af18efa5c47c7272b020d7de7f9e6f17ad7ec
kishor-14112000 <kishorkumar14112@gmail.com>
2024-11-25 19:56:52 +0530

    changes-1

--- src/authentication/auth.module.ts
+++ src/authentication/auth.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
+import { UserController } from '../users/user.contoller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ManageUsers } from '../entities/manage-users.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { UserService } from 'src/users/user.service';
 
 @Module({
     imports:[
+        TypeOrmModule.forFeature([ManageUsers]),
         PassportModule,
         JwtModule.register({
             secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: '8h' }
         })
     ],
-    providers: [],
-    controllers: []
+    providers: [UserService],
+    controllers: [UserController]
 })
 
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:4200',
-    methods: 'GET, POST, PUT, DELETE',
+    methods: 'GET, POST, PUT, PATCH, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
   app.useGlobalPipes(new ValidationPipe());
